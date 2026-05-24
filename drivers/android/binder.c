993e2c69e8b52e447f4b9cef46c5d8d71b524bfb
stijn <sbaselmans1@gmail.com>
2026-05-24 07:58:50 -0400

    binder: fix missing function parameters

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5905,6 +5905,10 @@ static void print_binder_transaction_ilocked(struct seq_file *m,
 }
 
 static void print_binder_work_ilocked(struct seq_file *m,
+					 struct binder_proc *proc,
+					 const char *prefix,
+					 const char *transaction_prefix,
+					 struct binder_work *w)
 {
 	struct binder_node *node;
 	struct binder_transaction *t;
