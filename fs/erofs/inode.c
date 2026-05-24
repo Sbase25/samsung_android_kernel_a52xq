79e5ae0d0d422e5a18696795fec4cb3f338fe4fc
stijn <sbaselmans1@gmail.com>
2026-05-24 07:36:43 -0400

    erofs: fix symlink detection code

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -136,6 +136,12 @@ static int erofs_fill_symlink(struct inode *inode, void *data,
 	loff_t off;
 	char *lnk;
 
+	m_pofs += vi->xattr_isize;
+	/* check if it cannot be handled with fast symlink scheme */
+	if (vi->datalayout != EROFS_INODE_FLAT_INLINE || inode->i_size < 0 ||
+	    check_add_overflow((loff_t)m_pofs, inode->i_size, &off) ||
+	    off > i_blocksize(inode)) {
+
 		inode->i_op = &erofs_symlink_iops;
 		return 0;
 	}
